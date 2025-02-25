7382d3d89efd2022f906d83d74149e3eb1bdde36
OlexandrSolo <coldfellow20111@gmail.com>
2025-02-25 20:43:54 +0200

    finished base cfg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// src/index.js
-
-import { startServer } from './server.js';
+import { startServer } from "./server.js";
 
 startServer();
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
-// src/server.js
-
 import express from 'express';
-import pino from 'pino-http';
+
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 import { getEnvVar } from './utils/getEnvVar.js';
+import { logger } from './middlewares/logger.js';
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -13,33 +16,12 @@ export const startServer = () => {
 
     app.use(express.json());
     app.use(cors());
+    app.use(cookieParser());
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
+    app.use(logger);
 
-    app.get('/', (req, res) => {
-        res.json({
-            message: 'Hello world!',
-        });
-    });
-
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
-
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    app.use(notFoundHandler);
+    app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
