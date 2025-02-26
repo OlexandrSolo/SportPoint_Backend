97898c7e3d37b54edf0b4272b41544d14031ce0e
OlexandrSolo <coldfellow20111@gmail.com>
2025-02-26 20:27:42 +0200

    base conf

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,5 +1,12 @@
 import { model, Schema } from 'mongoose';
+import { handleSaveError, setupUpdateValidator } from './hooks.js';
 
 const userSchema = new Schema();
+
+userSchema.post('save', handleSaveError);
+
+userSchema.pre('findOneAndUpdate', setupUpdateValidator);
+
+userSchema.post('findOneAndUpdate', handleSaveError);
 
 export const UsersCollection = model("users", userSchema);
