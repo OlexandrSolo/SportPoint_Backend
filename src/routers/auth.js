1af772d775ae3cdf9ded93979464aa2e217af9ef
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2025-02-27 12:13:53 +0200

    logout

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import ctrlWrapper from '../utils/ctrlWrapper';
+import auth from '../middlewares/auth';
 
 //controllers
 import register from '../controllers/auth/register';
 import login from '../controllers/auth/login';
+import logout from '../controllers/auth/logout';
 
 // validation
 import userRegisterSchema from '../validation/auth/register';
@@ -14,4 +16,4 @@ const router = express.Router();
 
 router.post('/signup', userRegisterSchema, ctrlWrapper(register));
 router.post('/signin', userLoginSchema, ctrlWrapper(login));
-router.post('/logout', () => console.log('Logout'));
+router.post('/logout', auth, ctrlWrapper(logout));
