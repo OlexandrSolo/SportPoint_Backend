05d165b5d32f56e117909158c861a33c766efff6
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2025-02-28 11:13:07 +0200

    created refresh token

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,20 +2,25 @@ import express from 'express';
 
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import auth from '../middlewares/auth.js';
+import authRefresh from '../middlewares/authRefresh.js';
 
 //controllers
 import register from '../controllers/auth/register.js';
 import login from '../controllers/auth/login.js';
 import logout from '../controllers/auth/logout.js';
+import refreshToken from '../controllers/auth/refreshToken.js';
 
 // validation
 import userRegisterSchema from '../validation/auth/register.js';
 import userLoginSchema from '../validation/auth/login.js';
+
+
 
 const router = express.Router();
 
 router.post('/signup', userRegisterSchema, ctrlWrapper(register));
 router.post('/signin', userLoginSchema, ctrlWrapper(login));
 router.post('/logout', auth, ctrlWrapper(logout));
+router.get('/refresh/current', authRefresh, ctrlWrapper(refreshToken));
 
 export default router;
