16015a852a9289d9817fe8d4ed9aaa833033b985
kotenkoI <iryna.herashchenko@gmail.com>
2025-03-05 01:19:55 +0200

    added review rating and sorting

--- src/routers/reviews.js
+++ src/routers/reviews.js
@@ -8,10 +8,12 @@ import {
 } from '../controllers/reviews/addReview.js';
 import auth from '../middlewares/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { reviewSchema } from '../validation/reviews/reviewsValidation.js';
 
 const router = express.Router();
 
-router.post('/', auth, ctrlWrapper(addReview)); 
+router.post('/', auth, validateBody(reviewSchema), ctrlWrapper(addReview)); 
 router.get('/', ctrlWrapper(getReviews));
 router.delete('/:id', auth, ctrlWrapper(deleteReview));
 router.patch('/:id/reply', auth, ctrlWrapper(replyToReview));
