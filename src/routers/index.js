08129c7e85dbda8b5dc3900d39f7eea930021640
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-03-05 20:13:39 +0100

    -added search by name endpoint  also create service and controller for this endpoint -added search endpoint to swagger docs

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import profileRouter from './userProfileRoute.js';
+import searchRouter from './searchRoutes.js';
 
 const router = Router();
 
 router.use('/profile', profileRouter);
+router.use('/search', searchRouter);
 
 export default router;
