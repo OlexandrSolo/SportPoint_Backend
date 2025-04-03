13a008c8a96b46dd7643586881aff1292e921800
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-04-03 16:53:58 +0200

    separate firstName lastName and made all necessary changes

--- src/services/searchByName.js
+++ src/services/searchByName.js
@@ -3,9 +3,12 @@ import { UserProfileModel } from '../db/models/UserProfileModel.js';
 export const searchByName = async (name) => {
   const regex = new RegExp(name.trim(), 'i');
 
-  const users = await UserProfileModel.find({
-    firstLastName: { $regex: regex },
+  const userFirst = await UserProfileModel.find({
+    firstName: { $regex: regex },
   });
-
+  const userLast = await UserProfileModel.find({
+    lastName: { $regex: regex },
+  });
+  const users = [...userFirst, ...userLast];
   return users;
 };
