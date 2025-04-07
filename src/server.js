7385064f473afd85151cd2fa2442d3b1b3ab6576
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2025-04-07 16:27:27 +0300

    correct 16:27

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:5175',
   'http://localhost:3000',
-  'https://sportpoint-backend.onrender.com'
 ];
 export const startServer = () => {
   const app = express();
