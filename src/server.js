28df27098573d3095b45e3acb9544d04305ab1e5
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2025-04-07 15:05:32 +0300

    correct change password

--- src/server.js
+++ src/server.js
@@ -18,6 +18,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
+  'http://localhost:3000',
+  'https://sportpoint-backend.onrender.com'
 ];
 export const startServer = () => {
   const app = express();
