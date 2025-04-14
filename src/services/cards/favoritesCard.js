d70b10487dfd182db36aba78fbbe390a827c7ee3
OlexandrSolo <coldfellow20111@gmail.com>
2025-04-14 21:06:31 +0300

    upadet login on card and favorite controller

--- src/services/cards/favoritesCard.js
+++ src/services/cards/favoritesCard.js
@@ -1,21 +1,6 @@
 import createHttpError from 'http-errors';
 import { UserProfileModel } from '../../db/models/UserProfileModel.js';
 
-// export const addToFavorites = async (_id, cardId) => {
-//     const user = await UserProfileModel.findOne({ userId: _id });
-//     console.log(user);
-//     if (!user) throw new createHttpError(404, `User ${_id} not found`);
-
-//     const cardExists = await UserProfileModel.exists({ _id: cardId });
-//     if (!cardExists) throw new createHttpError(404, `Card ${cardId} not found`);
-
-//     if (!user.favorite.some((fav) => fav.type === cardId)) {
-//         user.favorite.push({ type: cardId });
-//         await user.save();
-//     }
-//     return user.favorite;
-// };
-
 // Додавання в обрані
 export const addToFavorites = async (_id, cardId) => {
   const user = await UserProfileModel.findOne({ userId: _id });
@@ -50,16 +35,6 @@ export const addToFavorites = async (_id, cardId) => {
   return favorite;
 };
 
-// export const deleteFavoriteCard = async (_id, cardId) => {
-//     const user = await UserProfileModel.findOne({ userId: _id });
-//     if (!user) throw new createHttpError(404, `User ${_id} not found`);
-
-//     user.favorite = user.favorite.filter(id => id.toString() !== cardId);
-//     await user.save();
-
-//     return user.favorite;
-// };
-
 export const deleteFavoriteCard = async (_id, cardId) => {
   const user = await UserProfileModel.findOne({ _id: cardId });
   if (!user) throw new createHttpError(404, `User ${_id} not found`);
@@ -77,15 +52,6 @@ export const deleteFavoriteCard = async (_id, cardId) => {
 };
 
 // отримання списку обраних
-// export const getFavoriteCards = async (userId) => {
-
-//     const user = UserProfileModel.findById(userId);
-//     console.log(userId)
-//     if (!user) throw new createHttpError(404, `User ${userId} not found`);
-
-//     return user.favorite;
-// };
-
 export const getFavoriteCards = async (userId, role) => {
   const favoriteArray = [];
   const users = await UserProfileModel.find();
