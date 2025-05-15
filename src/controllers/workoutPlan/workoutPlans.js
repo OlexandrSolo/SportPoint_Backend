a8b0cbf22f6b13fcd379b5eb6118ddce23ab491d
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-05-15 22:30:43 +0200

    added get by id in workoutPlans

--- src/controllers/workoutPlan/workoutPlans.js
+++ src/controllers/workoutPlan/workoutPlans.js
@@ -3,73 +3,105 @@ import { ErrorsApp } from '../../constants/errors.js';
 import * as workoutPlanServices from '../../services/workoutPlan/workoutPlans.js';
 
 export const addWorkoutPlan = async (req, res) => {
-    const user = req.user;
-    const body = req.body;
+  const user = req.user;
+  const body = req.body;
 
-    if (user.role !== 'coach') {
-        return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
-    }
+  if (user.role !== 'coach') {
+    return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
+  }
 
-    const data = await workoutPlanServices.addWorkoutPlanService(user._id, body);
+  const data = await workoutPlanServices.addWorkoutPlanService(user._id, body);
 
-    res.status(201).json({
-        status: 201,
-        message: 'Successfully added!',
-        data
-    });
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully added!',
+    data,
+  });
 };
 
 export const updateWorkoutPlan = async (req, res) => {
-    const { user, params: { id }, body } = req;
+  const {
+    user,
+    params: { id },
+    body,
+  } = req;
 
-    if (user.role !== 'coach') {
-        return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
-    }
-    const updatedWorkout = await workoutPlanServices.updateWorkoutPlanService(id, user._id, body);
+  if (user.role !== 'coach') {
+    return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
+  }
+  const updatedWorkout = await workoutPlanServices.updateWorkoutPlanService(
+    id,
+    user._id,
+    body,
+  );
 
-    if (!updatedWorkout) {
-        throw createHttpError(404, "Workout plan not found or you don't have access");
-    }
+  if (!updatedWorkout) {
+    throw createHttpError(
+      404,
+      "Workout plan not found or you don't have access",
+    );
+  }
 
-    res.status(200).json({
-        status: 200,
-        message: 'Workout plane updated successfully!',
-        data: updatedWorkout,
-    });
+  res.status(200).json({
+    status: 200,
+    message: 'Workout plane updated successfully!',
+    data: updatedWorkout,
+  });
 };
 
 export const deleteWorkoutPlan = async (req, res) => {
-    const { user, params: { id } } = req;
+  const {
+    user,
+    params: { id },
+  } = req;
 
-    if (user.role !== 'coach') {
-        throw createHttpError(403, ErrorsApp.FORBIDDEN);
-    }
+  if (user.role !== 'coach') {
+    throw createHttpError(403, ErrorsApp.FORBIDDEN);
+  }
 
-    const deletedWorkoutPlane = await workoutPlanServices.deleteWorkoutPlanService(id, user._id);
+  const deletedWorkoutPlane =
+    await workoutPlanServices.deleteWorkoutPlanService(id, user._id);
 
-    if (!deletedWorkoutPlane) {
-        throw createHttpError(404, "Workout plane not found");
-    }
+  if (!deletedWorkoutPlane) {
+    throw createHttpError(404, 'Workout plane not found');
+  }
 
-    res.status(200).json({
-        status: 200,
-        message: 'Workout plan deleted successfully!',
-        data: deletedWorkoutPlane
-    });
+  res.status(200).json({
+    status: 200,
+    message: 'Workout plan deleted successfully!',
+    data: deletedWorkoutPlane,
+  });
 };
 
 export const getAllWorkoutPlans = async (req, res) => {
-    const user = req.user;
+  const user = req.user;
 
-    if (user.role !== 'coach') {
-        return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
-    };
+  if (user.role !== 'coach') {
+    return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
+  }
 
-    const allPlans = await workoutPlanServices.getAllWorkoutPlans(user._id);
+  const allPlans = await workoutPlanServices.getAllWorkoutPlans(user._id);
 
-    res.json({
-        status: 200,
-        message: "Successfully find workout plans",
-        data: allPlans,
-    });
+  res.json({
+    status: 200,
+    message: 'Successfully find workout plans',
+    data: allPlans,
+  });
+};
+export const getWorkoutPlan = async (req, res) => {
+  const user = req.user;
+  const { id } = req.params;
+  console.log(id);
+
+  if (user.role !== 'coach') {
+    return res.status(403).json({ message: ErrorsApp.FORBIDDEN });
+  }
+
+  const plan = await workoutPlanServices.getWorkoutPlanById(id);
+
+  res.json({
+    status: 200,
+    message: 'Successfully find workout plan',
+    data: plan,
+  });
 };
