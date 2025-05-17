894e8ba00409b67022980c22d1d1111d6052fa90
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-05-17 15:37:16 +0200

    added new url

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:5175',
   'http://localhost:3000',
-  'https://sport-point-front-end-psi.vercel.app',
+  'https://sport-point-front-end-xi.vercel.app',
 ];
 export const startServer = () => {
   const app = express();
