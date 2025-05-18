7e6353792ef270d8fe71a16c8be5f1d0bc8a64e3
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2025-05-18 15:15:29 +0300

    created verify email

--- src/services/auth/loginService.js
+++ src/services/auth/loginService.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import Auth from '../../db/models/auth.js';
 
 
-
 dotenv.config();
 
 const { SECRET_KEY } = process.env;
@@ -14,6 +13,7 @@ const loginService = async (user) => {
   const payload = {
   id: user._id,
 };
+
 
 const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '24h' });
 const refreshToken = jwt.sign(payload, SECRET_KEY, {
